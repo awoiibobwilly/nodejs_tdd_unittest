9294cc1ddd2356bae2c6f8b8686eec9e348972c8
awoiibob <awoiibobwilly@gmail.com>
2025-03-31 03:11:49 +0300

    Tested with Correct Figure, Test PASSED

--- test/multiply.test.js
+++ test/multiply.test.js
@@ -7,6 +7,6 @@ const multiply = require('../test.js'); // Ensure correct path
 
 describe('Multiplication Function', () => {
     it('should return 1 for multiply(1, 1)', () => {
-        assert.strictEqual(multiplyall(1, 1), 8);
+        assert.strictEqual(multiplyall(1, 1), 1);
     })
 });
