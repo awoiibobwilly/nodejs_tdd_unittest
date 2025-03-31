b388c89dfdd7357d92d0a749c8bbc6d8e25d562c
awoiibob <awoiibobwilly@gmail.com>
2025-03-31 03:28:44 +0300

    Trying with more numbers

--- test/multiply.test.js
+++ test/multiply.test.js
@@ -1,12 +1,33 @@
+//The Function that is invoked when for Mulplication
 function multiplyall(a, b) {
     return a * b;
 }
 
+// Testing with Mocha and Chai
 const assert = require('assert');
-const multiply = require('../test.js'); // Ensure correct path
+const multiply = require('../test.js');
 
-describe('Multiplication Function', () => {
-    it('should return 1 for multiply(1, 1)', () => {
+describe('The First Sets of Multiplication Function', () => {
+    it('Should return 1, when we multiply(1, 1)', () => {
         assert.strictEqual(multiplyall(1, 1), 1);
+    });
+    it('Should return -1, when we multiply(-1, 1)', () => {
+        assert.strictEqual(multiplyall(-1, 1), -1);
+    });
+    it('Should return 1, when we multiply(-1, -1)', () => {
+        assert.strictEqual(multiplyall(-1, -1), 1);
+    })
+});
+
+
+describe('The Second Sets of Multiplication Function', () => {
+    it('Should return 4, when we multiply(2, 2)', () => {
+        assert.strictEqual(multiplyall(2, 2), 4);
+    });
+    it('Should return -4, when we multiply(-2, 2)', () => {
+        assert.strictEqual(multiplyall(-2, 2), -4);
+    });
+    it('Should return 4, when we multiply(-2, -2)', () => {
+        assert.strictEqual(multiplyall(-2, -2), 4);
     })
 });
